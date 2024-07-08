8d9922dfe7653694a62fad9b33567ec07e3fa7a4
SHIVARAM KRISHNA <paulwalker8277@gmail.com>
2024-07-08 17:08:22 +0530

    all challenges responsive

--- Js/dashboard/challenges.js
+++ Js/dashboard/challenges.js
@@ -2,6 +2,8 @@
 import { db, get, ref, update } from "../../firebase.js";
 import { showResultModal } from "./dashboard.js";
 const user = JSON.parse(localStorage.getItem('user'));
+const gridBody = document.createElement('div');
+gridBody.classList.add('all_challenges_grid');
 
 // Display all challenges
 function readChallengesData() {
@@ -17,6 +19,12 @@ function readChallengesData() {
     tableBody.innerHTML = ''; // Clear existing table rows
     if (loader) tableBody.appendChild(loader); // Append the loader row to the table body
     if (noChallenges_row) tableBody.appendChild(noChallenges_row);
+
+    const grid_loader = document.querySelector('.grid_loader');
+    const grid_noChallenges = document.querySelector('.grid_noChallenges');
+    gridBody.innerHTML = '';
+    if(grid_loader) gridBody.appendChild(grid_loader);
+    if(grid_noChallenges) gridBody.appendChild(grid_noChallenges);
 
     get(challRef)
         .then((allChallengesSnapshot) => {
@@ -45,15 +53,12 @@ function readChallengesData() {
 // Function to display challenges
 function displayChallenges(allChallenges, enrolledChallengeIds) {
     const tableBody = document.getElementById('all_challenges_body');
-
     allChallenges.forEach((data) => {
         if (enrolledChallengeIds.includes(data.id)) {
             return; // Skip displaying this challenge if it is in the enrolled challenges list
         }
-
         const row = document.createElement('tr');
-
-        // Create category cell with dynamic span classes
+        // Create category cell with dynamic span classes for table
         const categoryCell = document.createElement('td');
         categoryCell.classList.add('category_col');
         const categorySpan = document.createElement('span');
@@ -65,7 +70,7 @@ function displayChallenges(allChallenges, enrolledChallengeIds) {
         }
         categoryCell.appendChild(categorySpan);
         row.appendChild(categoryCell);
-
+        
         const titleCell = document.createElement('td');
         titleCell.classList.add('title_content');
         titleCell.textContent = data.title || '';
@@ -107,13 +112,93 @@ function displayChallenges(allChallenges, enrolledChallengeIds) {
         row.appendChild(actionCell);
 
         tableBody.appendChild(row);
+
+        // card
+        const card = document.createElement('div');
+        card.classList.add('challenge_card');
+        // create card elements
+        const card_header = document.createElement('div');
+        card_header.classList.add('card_header');
+        const card_categoryCell = document.createElement('p');
+        if(data.category){
+            const firstWord = data.category.split(' ')[0];
+            card_categoryCell.classList.add('card_category', firstWord);
+            card_categoryCell.textContent = data.category || '';
+        }
+        card_header.appendChild(card_categoryCell);
+        const card_difficultyCell = document.createElement('p');
+        card_difficultyCell.classList.add('card_difficulty');
+        if (data.difficulty) {
+            card_difficultyCell.classList.add(data.difficulty);
+            card_difficultyCell.textContent = data.difficulty || '';
+        }
+        card_header.appendChild(card_difficultyCell);
+        card.appendChild(card_header);
+
+        const mobile_card_title = document.createElement('p');
+        mobile_card_title.classList.add('mobile_card_title');
+        mobile_card_title.textContent = data.title || '';
+        card.appendChild(mobile_card_title);
+
+        const mobile_card_desc = document.createElement('p');
+        mobile_card_desc.classList.add('mobile_card_desc');
+        mobile_card_desc.textContent = data.description || '';
+        card.appendChild(mobile_card_desc);
+
+        const timeframeWrapper = document.createElement('div');
+        timeframeWrapper.classList.add('timeframe_wrapper');
+        const postedonWrapper = document.createElement('div');
+        postedonWrapper.classList.add('postedon_wrapper');
+        const postedOnTitle = document.createElement('p');
+        postedOnTitle.classList.add('timeframe_title');
+        postedOnTitle.textContent = 'Posted On:';
+        const postedOnContent = document.createElement('p');
+        postedOnContent.classList.add('timeframe_content');
+        postedOnContent.id = 'card_PostedOn';
+        postedOnContent.textContent = data.postedOn || ''; // Initial data
+        const timeframeLine = document.createElement('div');
+        timeframeLine.classList.add('timeframe_line');
+        const endsonWrapper = document.createElement('div');
+        endsonWrapper.classList.add('endson_wrapper', 'text-end');
+        const endsOnTitle = document.createElement('p');
+        endsOnTitle.classList.add('timeframe_title');
+        endsOnTitle.textContent = 'Ends On:';
+        const endsOnContent = document.createElement('p');
+        endsOnContent.classList.add('timeframe_content');
+        endsOnContent.id = 'card_EndsOn';
+        endsOnContent.textContent = data.endsOn || ''; // Initial data
+        // Append elements
+        postedonWrapper.appendChild(postedOnTitle);
+        postedonWrapper.appendChild(postedOnContent);
+        endsonWrapper.appendChild(endsOnTitle);
+        endsonWrapper.appendChild(endsOnContent);
+        timeframeWrapper.appendChild(postedonWrapper);
+        timeframeWrapper.appendChild(timeframeLine);
+        timeframeWrapper.appendChild(endsonWrapper);
+        card.appendChild(timeframeWrapper);
+
+        const card_action_btn = document.createElement('div');
+        card_action_btn.classList.add('card_view_more_btn');
+        card_action_btn.innerHTML = '<span>View More</span><img src="../../assets/icons/arrow_icon.png" alt="" class="arrow_icon">';
+        card.addEventListener("click", () => showChallengeModal(data));
+        card.appendChild(card_action_btn);
+        
+        gridBody.appendChild(card);
     });
+    if(window.innerWidth<1112){
+        appendGridContent();
+    }
     // Call countVisibleRows function after displaying
     countVisibleRows(1);
 
     $('.loader_row').hide();
+    $('.grid_loader').hide();
 }
 
+function appendGridContent(){
+    const grid_content_container = document.querySelector(".all_challenges_grid_container");
+    grid_content_container.appendChild(gridBody);
+}
 
 // show challenge details
 function showChallengeModal(data) {
@@ -207,21 +292,35 @@ function filterChallengesByCategory(categoryName) {
 }
 
 // count countVisibleRows
-function countVisibleRows(reduce) {
+function countVisibleRows(reduce = 0) {
     $('#noChallenges').hide();
-    var visibleRows = $('#all_challenges_body tr:visible').length - reduce;
+    let visibleRows = 0;    
+    if (window.innerWidth > 1112) {
+        visibleRows = $('#all_challenges_body tr:visible').length - reduce;
+    } else {
+        visibleRows = $('.challenge_card').length;
+    }
     document.getElementById("challenges_count").textContent = visibleRows;
-    if(visibleRows === 0){
+    if (visibleRows === 0 && window.innerWidth>1112) {
         $('#noChallenges').show();
-    }else{
+    } else {
         $('#noChallenges').hide();
     }
+
+    if(visibleRows === 0 && window.innerWidth<1112){
+        $('#grid_noChallenges').show();
+    }else{
+        $('#grid_noChallenges').hide();
+    }
 }
+window.addEventListener('resize', () => {
+    countVisibleRows();
+    appendGridContent();
+});
 // Document Ready Function
 $(document).ready(function () {
     // Show loader initially
     $('#loader_row').show();
-
     $('.category_card').click(function () {
         // Show loader when category card is clicked
         $('#loader_row').show();
